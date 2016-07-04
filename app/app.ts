3190761e442de3b67a5c9977a6a7cca133a0e9fa
Manuel Moya <manuel.moya.roldan@gmail.com>
2016-07-04 21:14:53 +0200

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: '/app/app.html'
+    //template: '<div>app</div>',
+    //template: `<h2> My Skills are : {{mySkill }}</h2>`
+})
+
+export class AppComponent {
+
+    constructor() {
+
+    }
+}
